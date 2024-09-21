7db96d43e811c286e5099ca001ddcb5bd18e4658
Isha Negi <itsishanegi@gmail.com>
2024-09-22 02:20:52 +0530

    Add files via upload

---
+++ table-of-any-number.java
@@ -0,0 +1,15 @@
+public class Main{
+    
+public static void main(String[]args){
+     
+int n =5;
+       
+for(int a =1;a<=10;a++){
+       
+System.out.printf("%d X %d = %d\n",n,a,n*a);
+       
+}
+ 
+}
+
+} 
